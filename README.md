9223628eeeb8baf6a10cb2bce118351da7e45389
Scott Moore <me@scottwillmoore.au>
2023-06-12 19:22:53 +1000

    Create Rust project

---
+++ README.md
@@ -0,0 +1,8 @@
+# Solitaire
+
+> The best information about the winnability of Klondike concerns a modified version of the game called "Thoughtful Solitaire" or "Thoughtful Klondike", in which the location of all 52 cards is known.
+
+## References
+
+- https://en.wikipedia.org/wiki/Solitaire
+- https://en.wikipedia.org/wiki/Klondike_(solitaire)
---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    fenix.inputs.nixpkgs.follows = "nix-packages";
+    fenix.url = "github:nix-community/fenix";
+
+    flake-utilities.url = "github:numtide/flake-utils";
+
+    nix-packages.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  outputs = {
+    fenix,
+    flake-utilities,
+    nix-packages,
+    ...
+  }:
+    flake-utilities.lib.eachDefaultSystem (system: let
+      packages = import nix-packages {
+        inherit system;
+        overlays = [
+          fenix.overlays.default
+        ];
+      };
+    in {
+      devShells.default = packages.mkShell {
+        nativeBuildInputs = [
+          packages.fenix.stable.toolchain
+        ];
+      };
+    });
+}
