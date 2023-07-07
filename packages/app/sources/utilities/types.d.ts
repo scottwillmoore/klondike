e1d41b36dbb7ddaf7a30594f92ee201b34c1d37b
Scott Moore <me@scottwillmoore.au>
2023-07-07 16:14:47 +1000

    Update Prettier to latest major version

--- packages/app/sources/utilities/types.d.ts
+++ packages/app/sources/utilities/types.d.ts
@@ -1,12 +1,4 @@
-// https://vitest.dev/guide/testing-types.html
-// https://github.com/sindresorhus/type-fest/blob/main/test-d/is-any.ts
-// https://github.com/SamVerschueren/tsd
-
-// https://dev.to/ecyrbe/how-to-unit-test-your-typescript-utility-types-3cnm
-
-// https://stackoverflow.com/questions/53807517/how-to-test-if-two-types-are-exactly-the-same
-// https://github.com/aleclarson/spec.ts/blob/master/index.d.ts
-// https://github.com/garronej/tsafe
+// https://github.com/sindresorhus/type-fest
 
 export type Primitive =
 	| bigint
@@ -17,19 +9,22 @@ export type Primitive =
 	| symbol
 	| undefined;
 
+// https://github.com/sindresorhus/type-fest/blob/main/source/is-any.d.ts
 // https://stackoverflow.com/a/49928360/7349225
-export type IsAny<T> = false;
+export type IsAny<T> = 0 extends 1 & T ? true : false;
 
 // https://github.com/microsoft/TypeScript/issues/31751#issuecomment-498526919
+// https://github.com/sindresorhus/type-fest/blob/main/source/is-never.d.ts
 // https://stackoverflow.com/a/53984913/7349225
 // https://www.zhenghao.io/posts/ts-never#how-to-check-for-never
-export type IsNever<T> = T extends never ? true : false;
+export type IsNever<T> = [T] extends [never] ? true : false;
 
-type A = IsNever<0>; // =>
-
-type B = IsNever<never>; // =>
-
-export type IsUnknown<T> = false;
+// https://github.com/sindresorhus/type-fest/blob/main/source/is-unknown.d.ts
+export type IsUnknown<T> = unknown extends T
+	? IsAny<T> extends false
+		? true
+		: false
+	: false;
 
 export type IsStringLiteral<T> = false;
 export type IsNumberLiteral<T> = false;
